01f3f86419ff116a4728338a1995d3e0ab73c827
Grayson Crozier <77633076+grayson40@users.noreply.github.com>
2023-04-14 19:39:42 -0400

    working input driver

--- lkm_pa2_in.c
+++ lkm_pa2_in.c
@@ -1,3 +1,10 @@
+/**
+ * File:    lkm_pa2_in.c
+ * Adapted for Linux 5.15 by: John Aedo
+ * Class:   COP4600-SP23
+ * Group members: Sam Durkee, Grayson Crozier, and Chance Rupnow
+ */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/device.h>
@@ -6,23 +13,29 @@
 #include <linux/uaccess.h>
 #include <linux/mutex.h>
 
-#define DEVICE_NAME "lkm_pa2-in"
-#define CLASS_NAME "char"
+#define DEVICE_NAME "lkm_pa2_in"
+#define CLASS_NAME "in_char"
 #define BUFFER_LENGTH 1024
 
+MODULE_INFO(name, "lkm_pa2_in");
+MODULE_INFO(description, "Input Kernel Module");
+MODULE_INFO(version, "0.1");
+MODULE_INFO(author, "The homies");
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("The homies");
-MODULE_DESCRIPTION("Input Kernel Module");
-MODULE_VERSION("0.1");
 
 static int major_number;
 static int numberOpens = 0;
 static struct class *lkm_inputClass = NULL;
 static struct device *lkm_inputDevice = NULL;
 
-extern char shared_buffer[BUFFER_LENGTH];
-extern short sizeOfMessage;
-extern struct mutex shared_buffer_mutex;
+// Define shared variables
+char shared_buffer[BUFFER_LENGTH];
+short sizeOfMessage;
+struct mutex shared_buffer_mutex;
+
+EXPORT_SYMBOL(shared_buffer);
+EXPORT_SYMBOL(sizeOfMessage);
+EXPORT_SYMBOL(shared_buffer_mutex);
 
 static int open(struct inode *, struct file *);
 static int close(struct inode *, struct file *);
@@ -35,9 +48,12 @@ static struct file_operations fops = {
     .write = write,
 };
 
-int init_module(void)
+static int __init lkm_pa2_in_init(void)
 {
-    printk(KERN_INFO "%s: installing output module.\n", DEVICE_NAME);
+    printk(KERN_INFO "%s: installing input module.\n", DEVICE_NAME);
+
+    // Init mutex
+    mutex_init(&shared_buffer_mutex);
 
     // Allocate a major number for the device.
     major_number = register_chrdev(0, DEVICE_NAME, &fops);
@@ -49,35 +65,36 @@ int init_module(void)
     printk(KERN_INFO "%s: registered correctly with major number %d\n", DEVICE_NAME, major_number);
 
     // Register the device class
-    deviceClass = class_create(THIS_MODULE, CLASS_NAME);
-    if (IS_ERR(deviceClass))
+    lkm_inputClass = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(lkm_inputClass))
     { // Check for error and clean up if there is
         unregister_chrdev(major_number, DEVICE_NAME);
         printk(KERN_ALERT "Failed to register device class\n");
-        return PTR_ERR(deviceClass); // Correct way to return an error on a pointer
+        return PTR_ERR(lkm_inputClass); // Correct way to return an error on a pointer
     }
     printk(KERN_INFO "%s: device class registered correctly\n", DEVICE_NAME);
 
     // Register the device driver
-    deviceDriver = device_create(deviceClass, NULL, MKDEV(major_number, 0), NULL, DEVICE_NAME);
-    if (IS_ERR(deviceDriver))
+    lkm_inputDevice = device_create(lkm_inputClass, NULL, MKDEV(major_number, 0), NULL, DEVICE_NAME);
+    if (IS_ERR(lkm_inputDevice))
     {                               // Clean up if there is an error
-        class_destroy(deviceClass); // Repeated code but the alternative is goto statements
+        class_destroy(lkm_inputClass); // Repeated code but the alternative is goto statements
         unregister_chrdev(major_number, DEVICE_NAME);
         printk(KERN_ALERT "Failed to create the device\n");
-        return PTR_ERR(deviceDriver);
+        return PTR_ERR(lkm_inputDevice);
     }
     printk(KERN_INFO "%s: device class created correctly\n", DEVICE_NAME); // Made it! device was initialized
 
     return 0;
 }
 
-void cleanup_module(void)
+static void __exit lkm_pa2_in_exit(void)
 {
     printk(KERN_INFO "%s: removing input module.\n", DEVICE_NAME);
-    device_destroy(lkmasg1Class, MKDEV(major_number, 0)); // remove the device
-    class_unregister(lkmasg1Class);                       // unregister the device class
-    class_destroy(lkmasg1Class);                          // remove the device class
+    mutex_destroy(&shared_buffer_mutex);                    // remove mutex
+    device_destroy(lkm_inputClass, MKDEV(major_number, 0)); // remove the device
+    class_unregister(lkm_inputClass);                       // unregister the device class
+    class_destroy(lkm_inputClass);                          // remove the device class
     unregister_chrdev(major_number, DEVICE_NAME);         // unregister the major number
     printk(KERN_INFO "%s: Goodbye from the LKM!\n", DEVICE_NAME);
     unregister_chrdev(major_number, DEVICE_NAME);
@@ -99,11 +116,12 @@ static int close(struct inode *inodep, struct file *filep)
 
 static ssize_t write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
 {
+    int bytes_to_write;
     printk(KERN_INFO "%s: waiting on lock.\n", DEVICE_NAME);
     mutex_lock(&shared_buffer_mutex);
 
     printk(KERN_INFO "%s: acquiring lock.\n", DEVICE_NAME);
-    int bytes_to_write = min(len, (size_t)(BUFFER_LENGTH - sizeOfMessage));
+    bytes_to_write = min(len, (size_t)(BUFFER_LENGTH - sizeOfMessage));
 
     if (bytes_to_write < len)
     {
@@ -120,3 +138,6 @@ static ssize_t write(struct file *filep, const char *buffer, size_t len, loff_t 
     printk(KERN_INFO "%s: wrote %d bytes.\n", DEVICE_NAME, bytes_to_write);
     return bytes_to_write;
 }
+
+module_init(lkm_pa2_in_init);
+module_exit(lkm_pa2_in_exit);
